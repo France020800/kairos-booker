9f7bed5a5da09865d9b9712060b8f8b051a9615c
samoorai <hirotakatech@gmail.com>
2022-03-03 17:56:25 +0100

    Added /matricola command to save username into the db

--- src/main/java/com/guglielmo/kairosbookerspring/bot/KairosBot.java
+++ src/main/java/com/guglielmo/kairosbookerspring/bot/KairosBot.java
@@ -2,11 +2,24 @@ package com.guglielmo.kairosbookerspring.bot;
 
 import com.github.kshashov.telegram.api.TelegramMvcController;
 import com.github.kshashov.telegram.api.bind.annotation.BotController;
+import com.github.kshashov.telegram.api.bind.annotation.BotPathVariable;
+import com.github.kshashov.telegram.api.bind.annotation.request.MessageRequest;
+import com.guglielmo.kairosbookerspring.db.user.User;
+import com.guglielmo.kairosbookerspring.db.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @BotController
 public class KairosBot implements TelegramMvcController {
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public String getToken() {
-        return null;
+        return "5244556196:AAGhj7N-qcZBjR1_B9rmsaAgIn1rwxlSeYE";
+    }
+
+    @MessageRequest("/matricola {username:[0-9]{7}}")
+    public String setMatricola(@BotPathVariable("username") String username) {
+        userRepository.save(User.builder().username(username).build());
+        return "Matricola "+username+" salvata";
     }
 }
