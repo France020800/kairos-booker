a8a242351949f2ab303e04fb6f3744f3e6d5c502
samoorai <hirotakatech@gmail.com>
2022-03-04 00:20:08 +0100

    Added prenota command to book a lesson and tested menu

--- src/main/java/com/guglielmo/kairosbookerspring/bot/KairosBotRequestHandler.java
+++ src/main/java/com/guglielmo/kairosbookerspring/bot/KairosBotRequestHandler.java
@@ -4,9 +4,16 @@ import com.github.kshashov.telegram.api.TelegramMvcController;
 import com.github.kshashov.telegram.api.bind.annotation.BotController;
 import com.github.kshashov.telegram.api.bind.annotation.BotPathVariable;
 import com.github.kshashov.telegram.api.bind.annotation.request.MessageRequest;
+import com.guglielmo.kairosbookerspring.Booker;
 import com.guglielmo.kairosbookerspring.db.user.User;
 import com.guglielmo.kairosbookerspring.db.user.UserRepository;
+import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Chat;
+import com.pengrad.telegrambot.model.request.KeyboardButton;
+import com.pengrad.telegrambot.model.request.ParseMode;
+import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
+import com.pengrad.telegrambot.request.BaseRequest;
+import com.pengrad.telegrambot.request.SendMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,9 +29,11 @@ public class KairosBotRequestHandler implements TelegramMvcController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    private String botToken="5244556196:AAGhj7N-qcZBjR1_B9rmsaAgIn1rwxlSeYE";
+
     @Override
     public String getToken() {
-        return "5244556196:AAGhj7N-qcZBjR1_B9rmsaAgIn1rwxlSeYE";
+        return botToken;
     }
 
     @MessageRequest("/matricola {matricola}")
@@ -48,12 +57,32 @@ public class KairosBotRequestHandler implements TelegramMvcController {
         final Optional<User> optionalUser = userRepository.findByChadId(chat.id());
         if (optionalUser.isPresent()) {
             final User user = optionalUser.get();
-            user.setPassword(passwordEncoder.encode(password));
+//            user.setPassword(passwordEncoder.encode(password));
+            user.setPassword(password);
             userRepository.save(user);
             return "Password cifrata e salvata con successo";
         }
         return "Imposta prima la matricola";
     }
+
+
+    @MessageRequest("/prenota")
+    public BaseRequest book(Chat chat) {
+        final Optional<User> optionalUser = userRepository.findByChadId(chat.id());
+        if (optionalUser.isPresent()) {
+            final User user = optionalUser.get();
+            new Booker().book(user.getMatricola(),user.getPassword());
+            final ReplyKeyboardMarkup testMenu = new ReplyKeyboardMarkup(new KeyboardButton("Test"));
+            final SendMessage request = new SendMessage(user.getChadId(), "")
+                    .parseMode(ParseMode.HTML)
+                    .disableWebPagePreview(true)
+                    .disableNotification(true)
+                    .replyMarkup(testMenu);
+            return request;
+        }
+        return null;
+    }
+
 
     private boolean isMatricolaValid(String matricola) {
         try {
