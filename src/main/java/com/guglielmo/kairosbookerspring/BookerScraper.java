2e1894206b7aee6598263f47e06fdb74b310b0d7
France020800 <79871809+France020800@users.noreply.github.com>
2022-03-23 08:43:21 +0100

    Test...

--- src/main/java/com/guglielmo/kairosbookerspring/BookerScraper.java
+++ src/main/java/com/guglielmo/kairosbookerspring/BookerScraper.java
@@ -2,16 +2,14 @@ package com.guglielmo.kairosbookerspring;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.WebClientOptions;
-import com.gargoylesoftware.htmlunit.html.DomNode;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.*;
 
 import java.io.IOException;
 import java.util.List;
 
 public class BookerScraper {
 
-    private WebClient webClient;
+    private final WebClient webClient;
 
     public BookerScraper() {
         this.webClient = new WebClient();
@@ -25,6 +23,7 @@ public class BookerScraper {
         String kairosBookingPage = "https://kairos.unifi.it/agendaweb/index.php?view=prenotalezione&include=prenotalezione&_lang=it";
 
         final HtmlPage page = webClient.getPage(kairosFormPage);
+        System.out.println("*********************** " + page.getTitleText() + " **********************");
 
         // Click conditions buttons
         String privacySliderSelector = "#main-content > div.main-content-body > div.container > div:nth-child(2) > div.col-lg-6 > div > div:nth-child(5) > div.col-xs-3 > label > span";
@@ -37,21 +36,23 @@ public class BookerScraper {
         // Click login button
         final HtmlElement login = page.querySelector("#oauth_btn");
         HtmlPage loginPage = login.click();
-
-        final HtmlElement usernameInput = loginPage.querySelector("#username");
-        final HtmlElement passwordInput = loginPage.querySelector("#password");
+        System.out.println("**************************** " + loginPage.getTitleText() + " **************************");
 
         // Insert username e password
-        usernameInput.setNodeValue(username);
-        passwordInput.setNodeValue(password);
+        HtmlTextInput user = (HtmlTextInput) loginPage.getElementById("username");
+        HtmlTextInput pass = (HtmlTextInput) loginPage.getElementById("password");
+        user.setText(username);
+        pass.setText(password);
 
         // Login button
-        final HtmlElement loginButton = loginPage.querySelector("body > div > div > div > div.column.one > form > div:nth-child(5) > button");
+        final HtmlElement loginButton = loginPage.querySelector("body > div > div > div > div.column.one > form > div:nth-child(5)");
         final HtmlPage optionsPage = loginButton.click();
+        System.out.println("********************* " + optionsPage.getTitleText() + " *********************");
 
         // Prenota e gestisci il tuo posto a lezione button
         final HtmlElement bookingButton = optionsPage.querySelector("#main-content > div.main-content-body > div:nth-child(8) > div:nth-child(3) > a > div > div.colored-box-section-1 > span");
         final HtmlPage intermediatePage = bookingButton.click();
+
 
         // Nuova prenotazione button
         final HtmlElement newBook = intermediatePage.querySelector("#menu_container > div:nth-child(1) > div > div.colored-box-section-1 > a > div");
